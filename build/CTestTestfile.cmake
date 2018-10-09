ddd920fe8025f39963020340d8812c9120ba525e
gunagya <gunagyamamak2@gmail.com>
2018-10-10 04:11:01 +0530

    removed build and devel folders

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kartikay/Desktop/wks/src
-# Build directory: /home/kartikay/Desktop/wks/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(comm_test)
--- build/comm_test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kartikay/Desktop/wks/src/comm_test
-# Build directory: /home/kartikay/Desktop/wks/build/comm_test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
