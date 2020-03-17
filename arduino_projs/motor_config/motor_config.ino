0e57d920b7c3d7ed75dfe03e189d85193c0309b4
gunagya <gunagyamamak2@gmail.com>
2020-03-17 13:22:22 +0530

    Final project

--- arduino_projs/motor_config/motor_config.ino
+++ arduino_projs/motor_config/motor_config.ino
@@ -2,15 +2,17 @@
 #include "std_msgs/Int16.h"
 
 bool l = false, r = false, di = false, du = false;
-int v1 = 0, v2 = 0, v3 = 0, v4 = 0;
-int leftMotor  = 11;
-int rightMotor = 10;
+int v1 = 0, v2 = 0, v3 = 0, v4 = 0; 
+#define leftMotorForw 11
+#define rightMotorForw 8
+#define leftMotorBack 12
+#define rightMotorBack 9
 
 ros::NodeHandle nh;
 
 void call_move(){
   if (l == true && r == true && di == true && du == true){
-    turn(v1, v2, v4, v3);
+    turn(v1, v2, v3, v4);
     l = false;
     r = false;
     di = false;
@@ -24,7 +26,6 @@ void intCallback1(const std_msgs::Int16& value){
     String sv1 = String(v1);
     l = true;
     call_move();
-    //nh.loginfo(sv1);
 }
 
 
@@ -33,7 +34,6 @@ void intCallback2(const std_msgs::Int16& value){
     String sv2 = String(v2);
     r = true;
     call_move();
-    //nh.loginfo(sv2);
 }
 
 
@@ -42,7 +42,6 @@ void intCallback3(const std_msgs::Int16& value){
     String sv3 = String(v3);
     du = true;
     call_move();
-    //nh.loginfo(sv3);
 }
 
 
@@ -51,66 +50,42 @@ void intCallback4(const std_msgs::Int16& value){
     String sv4 = String(v4);
     di = true;
     call_move();
-    //nh.loginfo(sv4);
 }
 
-ros::Subscriber<std_msgs::Int16> sub1("lspd", intCallback1);
-ros::Subscriber<std_msgs::Int16> sub2("rspd", intCallback2);
-ros::Subscriber<std_msgs::Int16> sub3("dir", intCallback3);
-ros::Subscriber<std_msgs::Int16> sub4("dur", intCallback4);
+ros::Subscriber<std_msgs::Int16> sub1("/leftMotorForw", intCallback1);
+ros::Subscriber<std_msgs::Int16> sub2("/leftMotorBack", intCallback2);
+ros::Subscriber<std_msgs::Int16> sub3("/rightMotorForw", intCallback3);
+ros::Subscriber<std_msgs::Int16> sub4("/rightMotorBack", intCallback4); 
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(leftMotor, OUTPUT);
-  pinMode(rightMotor, OUTPUT);
-  analogWrite(leftMotor, 128);
-  analogWrite(rightMotor, 128);
+  pinMode(leftMotorForw, OUTPUT);
+  pinMode(leftMotorBack, OUTPUT);
+  pinMode(rightMotorForw, OUTPUT);
+  pinMode(rightMotorBack, OUTPUT);
   nh.initNode();
   nh.subscribe(sub1);
   nh.subscribe(sub2);
   nh.subscribe(sub3);
-  nh.subscribe(sub4);
+  nh.subscribe(sub4); 
 }
 
 void loop() {
-//  // put your main code here, to run repeatedly:
-//  forward(75, 2000);
-//  //delay(2000);
-//  turn(75, 25, 2000, 1);
-//  delay(2000);
-//  turn(25, 75, 2000, 1);
-//  delay(2000);
     nh.spinOnce();
     delay(1);
 }
 
-void stahp(){
-  analogWrite(leftMotor, 127);
-  analogWrite(rightMotor, 127);
+void stahp(){             
+  digitalWrite(leftMotorForw, 0); 
+  digitalWrite(leftMotorBack, 0); 
+  digitalWrite(rightMotorForw, 0);
+  digitalWrite(rightMotorBack, 0);
 }
 
-void stahp_slow(){
-  analogWrite(leftMotor, 128);
-  analogWrite(rightMotor, 128);
-}
 
-void forward(int spd, int dur){
-  analogWrite(leftMotor, 128+spd);
-  analogWrite(rightMotor, 128+spd);
-  delay(dur);
-  stahp();
-}
-
-void turn(int lspd, int rspd, int dur, int dir) {
-  analogWrite(leftMotor, 128+(dir*lspd));
-  analogWrite(rightMotor, 128+(dir*rspd));
-  delay(dur);
-  stahp();
-}
-
-void backward(int spd, int dur){
-  analogWrite(leftMotor, 128-spd);
-  analogWrite(rightMotor, 128-spd);
-  delay(dur);
-  stahp();
+void turn(int lMF, int lMB, int rMF, int rMB) {   
+  digitalWrite(leftMotorForw, lMF);
+  digitalWrite(leftMotorBack, lMB);
+  digitalWrite(rightMotorForw, rMF);
+  digitalWrite(rightMotorBack, rMB);
+  delay(10);
 }
--- src/comm_test/src/key_to_ard.cpp
+++ src/comm_test/src/key_to_ard.cpp
@@ -1,11 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include "ros/ros.h"
-
-#include "std_msgs/MultiArrayLayout.h"
-#include "std_msgs/MultiArrayDimension.h"
-
 #include "std_msgs/Int16.h"
 
 ros::Publisher pub_1;
@@ -13,65 +8,49 @@ ros::Publisher pub_2;
 ros::Publisher pub_3;
 ros::Publisher pub_4;
 
-int max_spd = 52;
-int updatation_rate= 10;
-int turn_speed = 4;
-
-int min=1;
-int max=256;
-
-void publisher(std_msgs::Int16 lspd, std_msgs::Int16 rspd, std_msgs::Int16 dur, std_msgs::Int16 dir){
-    pub_1.publish(lspd);
-    pub_2.publish(rspd);
-    pub_3.publish(dur);
-    pub_4.publish(dir);
-    ROS_INFO("Published: [%d] [%d] [%d] [%d]", lspd.data, rspd.data, dur.data, dir.data);
+void publisher(std_msgs::Int16 leftMotorForw, std_msgs::Int16 leftMotorBack, std_msgs::Int16 rightMotorForw, std_msgs::Int16 rightMotorBack){
+    pub_1.publish(leftMotorForw);
+    pub_2.publish(leftMotorBack);
+    pub_3.publish(rightMotorForw);
+    pub_4.publish(rightMotorBack);
+    ROS_INFO("Published: [%d] [%d] [%d] [%d]", leftMotorForw.data, leftMotorBack.data, rightMotorForw.data, rightMotorBack.data);
 }
 
 void forward_down(){
-    std_msgs::Int16 lspd, rspd, dur, dir;
-    lspd.data = max_spd;
-    rspd.data = max_spd;
-    dur.data = updatation_rate;
-    dir.data = 1;
-    ROS_INFO("Published: [%d] [%d] [%d] [%d]", lspd.data, rspd.data, dur.data, dir.data);
-    publisher(lspd, rspd, dur, dir);
+    std_msgs::Int16 leftMotorForw, leftMotorBack, rightMotorForw, rightMotorBack;
+    leftMotorForw.data = 1;
+    leftMotorBack.data = 0;
+    rightMotorForw.data = 1;
+    rightMotorBack.data = 0;
+    ROS_INFO("Published: [%d] [%d] [%d] [%d]", leftMotorForw.data, leftMotorBack.data, rightMotorForw.data, rightMotorBack.data);
+    publisher(leftMotorForw, leftMotorBack, rightMotorForw, rightMotorBack);
 }
 
 void back_down(){
-    std_msgs::Int16 lspd, rspd, dur, dir;
-    lspd.data = max_spd;
-    rspd.data = max_spd;
-    dur.data = updatation_rate;
-    dir.data = -1;
-    publisher(lspd, rspd, dur, dir);
+    std_msgs::Int16 leftMotorForw, leftMotorBack, rightMotorForw, rightMotorBack;
+    leftMotorForw.data = 0;
+    leftMotorBack.data = 1;
+    rightMotorForw.data = 0;
+    rightMotorBack.data = 1;
+    publisher(leftMotorForw, leftMotorBack, rightMotorForw, rightMotorBack);
 }
 
 void right_down(){
-    std_msgs::Int16 lspd, rspd, dur, dir;
-    lspd.data = max_spd;
-    rspd.data = max_spd/turn_speed;
-    dur.data = updatation_rate;
-    dir.data = 1;
-    publisher(lspd, rspd, dur, dir);
+    std_msgs::Int16 leftMotorForw, leftMotorBack, rightMotorForw, rightMotorBack;
+    leftMotorForw.data = 1;
+    leftMotorBack.data = 0;
+    rightMotorForw.data = 0;
+    rightMotorBack.data = 1;
+    publisher(leftMotorForw, leftMotorBack, rightMotorForw, rightMotorBack);
 }
 
 void left_down(){
-    std_msgs::Int16 lspd, rspd, dur, dir;
-    lspd.data = max_spd/turn_speed;
-    rspd.data = max_spd;
-    dur.data = updatation_rate;
-    dir.data = 1;
-    publisher(lspd, rspd, dur, dir);
-}
-
-void key_up(){
-    std_msgs::Int16 lspd, rspd, dur, dir;
-    lspd.data = 0;
-    rspd.data = 0;
-    dur.data = updatation_rate;
-    dir.data = 1;
-    publisher(lspd, rspd, dur, dir);
+    std_msgs::Int16 leftMotorForw, leftMotorBack, rightMotorForw, rightMotorBack;
+    leftMotorForw.data = 0;
+    leftMotorBack.data = 1;
+    rightMotorForw.data = 1;
+    rightMotorBack.data = 0;
+    publisher(leftMotorForw, leftMotorBack, rightMotorForw, rightMotorBack);
 }
 
 void key_down_callback(const std_msgs::Int16::ConstPtr& value){
@@ -90,18 +69,23 @@ void key_down_callback(const std_msgs::Int16::ConstPtr& value){
     }
 }
 
-void key_up_callback(const std_msgs::Int16::ConstPtr& value){
-    key_up();
+void key_up_callback(const std_msgs::Int16::ConstPtr& value){    
+    std_msgs::Int16 leftMotorForw, leftMotorBack, rightMotorForw, rightMotorBack;
+    leftMotorForw.data = 0;
+    leftMotorBack.data = 0;
+    rightMotorForw.data = 0;
+    rightMotorBack.data = 0;
+    publisher(leftMotorForw, leftMotorBack, rightMotorForw, rightMotorBack);
 }
 
 int main(int argc, char **argv){
     ros::init(argc, argv, "arrayPublisher");
     ros::NodeHandle nh;
     
-    ros::Publisher pub_1 = nh.advertise<std_msgs::Int16>("/lspd", 100);
-    ros::Publisher pub_2 = nh.advertise<std_msgs::Int16>("/rspd", 100);
-    ros::Publisher pub_3 = nh.advertise<std_msgs::Int16>("/dur", 100);
-    ros::Publisher pub_4 = nh.advertise<std_msgs::Int16>("/dir", 100);
+    ::pub_1 = nh.advertise<std_msgs::Int16>("/leftMotorForw", 100);
+    ::pub_2 = nh.advertise<std_msgs::Int16>("/leftMotorBack", 100);
+    ::pub_3 = nh.advertise<std_msgs::Int16>("/rightMotorForw", 100);
+    ::pub_4 = nh.advertise<std_msgs::Int16>("/rightMotorBack", 100);
 
     ros::Subscriber sub1 = nh.subscribe("keydown_pub", 100, key_down_callback);
     ros::Subscriber sub2 = nh.subscribe("keyup_pub", 100, key_up_callback);
